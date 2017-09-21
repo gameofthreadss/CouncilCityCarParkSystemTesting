3361f98ff8df18fc082ddb3bf35fb4a10ff6d50f
radha chawada <radhachawada@gmail.com>
2017-09-22 07:38:16 +1000

    Added logic for testDeregister method
    Added logic for testDeregister method

--- test/bcccp/carpark/CarparkTest.java
+++ test/bcccp/carpark/CarparkTest.java
@@ -50,9 +50,9 @@ public class CarparkTest {
         System.out.println("deregister");
         ICarparkObserver observer = null;
         Carpark instance = null;
-//        instance.deregister(observer);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
+        instance.deregister(observer);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
     }
 
     /**
