1752196285b1f65a3849e9b3760ebca50ecf97eb
radha chawada <radhachawada@gmail.com>
2017-09-22 07:48:41 +1000

    Modified testToString method of UsageRecord.java
    Modified testToString method of UsageRecord.java

--- test/bcccp/tickets/season/UsageRecordTest.java
+++ test/bcccp/tickets/season/UsageRecordTest.java
@@ -88,12 +88,12 @@ public class UsageRecordTest {
     @Test
     public void testToString() {
         System.out.println("toString");
-//        UsageRecord instance = null;
-//        String expResult = "";
-//        String result = instance.toString();
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
+        UsageRecord instance = null;
+        String expResult = "";
+        String result = instance.toString();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
     }
     
 }
