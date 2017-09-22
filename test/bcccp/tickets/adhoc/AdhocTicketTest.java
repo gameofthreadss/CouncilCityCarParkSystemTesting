0bca761d548772b13e16eeb749adf37330aafdc1
Pratikk <patelpratik512@outlook.com>
2017-09-22 21:43:10 +1000

    created AdhockTicketTest Class for testing

---
+++ test/bcccp/tickets/adhoc/AdhocTicketTest.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bcccp.tickets.adhoc;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author swaminarayan
+ */
+public class AdhocTicketTest {
+    
+    public AdhocTicketTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getBarcode method, of class AdhocTicket.
+     */
+    @Test
+    public void testGetBarcode() {
+        System.out.println("getBarcode");
+        AdhocTicket instance = null;
+        String expResult = "";        
+       
+    }
+
+}
