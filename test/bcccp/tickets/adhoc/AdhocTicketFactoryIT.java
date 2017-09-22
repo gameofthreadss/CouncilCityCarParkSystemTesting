ebc2a9034d3e00913347b88085c9f3132348a8ae
Pratikk <patelpratik512@outlook.com>
2017-09-22 21:58:57 +1000

    Integration Test For AdhockTickeFactory CLass as entire class

---
+++ test/bcccp/tickets/adhoc/AdhocTicketFactoryIT.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bcccp.tickets.adhoc;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author swaminarayan
+ */
+public class AdhocTicketFactoryIT {
+    
+    public AdhocTicketFactoryIT() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of make method, of class AdhocTicketFactory.
+     */
+    @Test
+    public void testMake() {
+        System.out.println("make");
+        String carparkId = "";
+        int ticketNo = 0;
+      
+    }
+    
+}
