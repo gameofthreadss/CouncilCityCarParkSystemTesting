2019833287b15d79be74d2d462d6c492f20c715e
Pratikk <patelpratik512@outlook.com>
2017-09-22 21:53:10 +1000

    Unit Testing class Created to test AdhockDAO class methods. - Createticket() tested

---
+++ test/bcccp/tickets/adhoc/AdhocTicketDAOTest.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bcccp.tickets.adhoc;
+
+import java.util.List;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author swaminarayan
+ */
+public class AdhocTicketDAOTest {
+    
+    public AdhocTicketDAOTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of createTicket method, of class AdhocTicketDAO.
+     */
+    @Test
+    public void testCreateTicket() {
+        System.out.println("createTicket");
+        String carparkId = "";
+        AdhocTicketDAO instance = null;
+        IAdhocTicket expResult = null;
+      
+    }
+
+   
+}
